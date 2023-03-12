0b2f11a6ce4279d5cf785d867e456fa5d8edfa69
hosseinfallahi75 <hossein.fallahi75programmer@gmail.com>
2023-03-12 17:26:29 +0330

    teach 1 main

--- index.py
+++ index.py
@@ -1,1 +1,97 @@
-print("hello word")
+# print("2")
+
+ali = "fatemeh fallahi hossein akbari 3"
+numberr = 3
+
+floate = 4.333;a = '1234.5';b = int(float(a));c = float(b)
+test = ' '
+d=None
+# print(type(d))
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# if(a == b) :
+#  print("ali")
+
+# myString = str(23.5)
+# print(test)
+
+
+
+
+def helpmee(a,b) :  
+    # print(' i am to helo you',a,b) #print ali # asdasdas # asdasdasd            
+
+
+ helpmee(4,"asss")
+
+
+# finally
+# elif
+# lambda
+# else
+# if
+# def
+# class
+# for
+# return
+# try
+# while
+# nonlocal
+# is
+# for
+# from 
+# continue
+# True
+# None
+# False
+
+
+#    ^!@#$%
+
+
+# نام متغییر حتما باید با یه حرف شروع بشود
+
+# a = "a"
+# A = "A"
+
+# print(bool(a))
+
+# print(13>14)
+
+# +
+# -
+# *
+# /
+# %
+# //
+# **
+
+
+x = 15
+y =4
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x//y)
+# print(x**y)
+
+
+
+# x>y
+# x<y
+# x==y
+# x != y
+# x >= y
+# x <= y
+
+
+print('x > y is',x>y)
+print('x < y is',x<y)
+print('x == y is',x==y)
+print('x != y is',x!=y)
+print('x >= y is',x>=y)
+print('x <= y is',x<=y)
